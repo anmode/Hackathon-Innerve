9a318d21525a4bc39c38d699a88ce38f4c9a0008
Anmol <anmol2002.aa1@gmail.com>
2022-09-28 15:52:35 +0530

    modified js

--- sw.js
+++ sw.js
@@ -1,11 +1,6 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-    '/Portfolio/',
-    '/Portfolio/index.html',
-    '/style.css',
-    '/Portfolio/assets/img/mobile.png',
-    '/Portfolio/assets/img/favicon.png',
-    'https://fonts.googleapis.com/css?family=Lato:300,400,700'
+  
 ];
 // install event
 self.addEventListener('install', evt => {
